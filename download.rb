3f4ceaf8196a265e4f0e40eea8d5a7ed57687338
Julian Ronayi <duskent2@gmail.com>
2016-05-08 17:29:17 +0300

    Change api_key to personal app key

--- download.rb
+++ download.rb
@@ -3,10 +3,11 @@ require 'net/http'
 require 'open-uri'
 # Reading Last track id from file
 last_track_id = File.open('last_track.txt', 'r').readlines.first
+access_token = File.open('access_token.txt', 'r').readlines.first
 # Parse all tracks
 puts 'Getting All Tracks...'
 all_tracks = open 'https://api.vk.com/method/audio.get?'\
-  'access_token=f576b7c1441fc88564cb166f7d9e6b757039c549a3a71d2bfa258391a017c1f4b8d255b8967d14435ef6b&'\
+  "access_token=#{access_token}&"\
   'owner_id=28151710'
 puts 'Parsing data...'
 all_tracks = JSON.parse(all_tracks.read)
@@ -24,7 +25,7 @@ all_tracks['response'][0..track_index].each_with_index do |track, index|
   # Getting track info from json data
   subdomain = track['url'].split('//').last.split('/').first
   track_url = track['url'].split('net').last
-  track_name = "#{track['artist']} - #{track['title']}.mp3"
+  track_name = "#{track['artist'].gsub('/', '').gsub('\\', '').gsub('?', '')} - #{track['title'].gsub('/', '').gsub('\\', '').gsub('?', '')}.mp3"
   # Displaying progress
   puts "Downloading #{track_name}, #{tracks_to_download - index} left"
   # Creating request
